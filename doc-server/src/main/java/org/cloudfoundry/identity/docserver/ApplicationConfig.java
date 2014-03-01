ce16648ecddb8d4d459cac893e0c91635f08d9a2
Philip Kuryloski and Robert Gallagher <pair+pkuryloski+rgallagher@pivotallabs.com>
2014-02-28 16:45:15 -0800

    Doc server now barefoot
    Doc server runs standalone, or from the root project
    
    use 'mvn tomcat7:run -Pintegration' to launch it all.  visit /api-docs to view the docs

---
+++ doc-server/src/main/java/org/cloudfoundry/identity/docserver/ApplicationConfig.java
@@ -0,0 +1,34 @@
+package org.cloudfoundry.identity.docserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@ComponentScan(basePackages = "org.cloudfoundry.identity.docserver")
+@Configuration
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
--- doc-server/src/test/java/org/cloudfoundry/identity/docserver/DocumentationControllerTest.java
+++ doc-server/src/test/java/org/cloudfoundry/identity/docserver/DocumentationControllerTest.java
@@ -1,10 +1,9 @@
-package docserver;
+package org.cloudfoundry.identity.docserver;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationContextLoader;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -13,14 +12,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.hamcrest.Matchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(classes = Application.class, loader = SpringApplicationContextLoader.class)
-public class ApplicationTests {
+@ContextConfiguration(classes = ApplicationConfig.class)
+public class DocumentationControllerTest {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
