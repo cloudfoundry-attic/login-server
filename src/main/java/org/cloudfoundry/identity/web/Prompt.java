7826c213b57ec720e1e6d4915c89d23c326f81a7
Philip Kuryloski <pair+pkuryloski@pivotallabs.com>
2014-05-12 15:06:21 -0700

    Test Prompt object

--- src/main/java/org/cloudfoundry/identity/web/Prompt.java
+++ src/main/java/org/cloudfoundry/identity/web/Prompt.java
@@ -12,8 +12,6 @@
  *******************************************************************************/
 package org.cloudfoundry.identity.web;
 
-import org.springframework.util.StringUtils;
-
 /**
  * @author Dave Syer
  * 
@@ -36,41 +34,5 @@ public class Prompt {
 
     public String[] getDetails() {
         return new String[] { type, text };
-    }
-
-    public static Prompt valueOf(String text) {
-        if (!StringUtils.hasText(text)) {
-            return null;
-        }
-        String[] parts = text.split(":");
-        if (parts.length < 2) {
-            return null;
-        }
-        String name = parts[0].replaceAll("\"", "");
-        String[] values = parts[1].replaceAll("\"", "").replaceAll("\\[", "").replaceAll("\\]", "").split(",");
-        values = StringUtils.trimArrayElements(values);
-        return new Prompt(name, values[0], values[1]);
-    }
-
-    @Override
-    public String toString() {
-        return String.format("\"%s\":[\"%s\",\"%s\"]", name, type, text);
-    }
-
-    @Override
-    public int hashCode() {
-        return 31 + toString().hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Prompt other = (Prompt) obj;
-        return toString().equals(other.toString());
     }
 }
