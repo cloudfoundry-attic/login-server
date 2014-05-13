9227bdfe14f30decec8f5577463ccc43b9e96d89
Filip Hanik and Philip Kuryloski <pair+fhanik+pkuryloski@pivotallabs.com>
2014-05-13 09:59:45 -0700

    Add deploy scripts
    [#71195408] https://www.pivotaltracker.com/story/show/71195408

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -2,5 +2,5 @@
 
 cd `dirname $0`/..
 
-mvn versions:update-parent -DgenerateBackupPoms=false -DparentVersion=$1
-mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$2
+mvn versions:update-parent -DgenerateBackupPoms=false -DparentVersion=$2
+mvn versions:set -DgenerateBackupPoms=false -DnewVersion=$1
