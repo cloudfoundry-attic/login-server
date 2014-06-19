3c66e494827f282db877670b45577c357c62e93f
Rob Gallagher <rgallagher@pivotallabs.com>
2014-06-19 16:17:31 -0700

    Do not expose env vars in output

--- scripts/travis/auto-bump-uaa.sh
+++ scripts/travis/auto-bump-uaa.sh
@@ -9,4 +9,7 @@ git checkout `git tag -l 'travis-success-*' | sort -n | tail -n 1`
 cd ..
 git add uaa
 echo `git commit -m "Auto-updating UAA to latest develop version"`
+
+set +x
+
 git push https://cf-identity-eng:$GH_TOKEN@github.com/cloudfoundry/login-server.git develop
