86c77d9a1a5b9cfd9b423a7332fb7872dee9fcca
Rob Gallagher <rgallagher@pivotallabs.com>
2014-06-19 16:14:29 -0700

    Use backward-compatible way of sorting tags

--- scripts/travis/auto-bump-uaa.sh
+++ scripts/travis/auto-bump-uaa.sh
@@ -5,7 +5,7 @@ cd `dirname $0`/../..
 set -x
 
 cd uaa
-git checkout `git tag -l --sort=version:refname 'travis-success-*' | tail -n 1`
+git checkout `git tag -l 'travis-success-*' | sort -n | tail -n 1`
 cd ..
 git add uaa
 echo `git commit -m "Auto-updating UAA to latest develop version"`
