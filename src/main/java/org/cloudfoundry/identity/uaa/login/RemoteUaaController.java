a64e7fa9be0dedc3d4b543cb1a9adb5cff8ac3e8
Rob Gallagher <rgallagher@pivotallabs.com>
2014-06-27 16:55:22 -0700

    Add flag to disable local signups - Defaults to enabled - Custom signup link is still shown if configured [#73324702] https://www.pivotaltracker.com/story/show/73324702

--- src/main/java/org/cloudfoundry/identity/uaa/login/RemoteUaaController.java
+++ src/main/java/org/cloudfoundry/identity/uaa/login/RemoteUaaController.java
@@ -191,7 +191,15 @@ public class RemoteUaaController extends AbstractControllerInfo {
         model.putAll(getLoginInfo(getUaaBaseUrl() + "/" + path, getRequestHeaders(headers)));
         model.put("links", getLinksInfo());
         if (principal == null) {
-            model.put("createAccountLink", environment.getProperty("links.signup", "/accounts/new"));
+            String customSignupLink = environment.getProperty("links.signup");
+            if (customSignupLink != null) {
+                model.put("createAccountLink", customSignupLink);
+            } else {
+                boolean localSignupsEnabled = !"false".equalsIgnoreCase(environment.getProperty("login.signupsEnabled"));
+                if (localSignupsEnabled) {
+                    model.put("createAccountLink", "/accounts/new");
+                }
+            }
             return "login";
         }
         return "home";
