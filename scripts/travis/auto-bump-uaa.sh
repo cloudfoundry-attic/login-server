d964365b8848f83b4ab699705715ba53f67278a3
Rob Gallagher <rgallagher@pivotallabs.com>
2014-07-14 14:38:03 -0700

    Adjust travis uaa submodule auto-bump script
    Script will only bump if the last successfull UAA commit is ahead
    of the current submodule pointer
    
    Signed-off-by: Philip Kuryloski <pkuryloski@pivotallabs.com>

--- scripts/travis/auto-bump-uaa.sh
+++ scripts/travis/auto-bump-uaa.sh
@@ -9,11 +9,20 @@ set -x
 
 git checkout develop
 cd uaa
-git checkout `git tag -l 'travis-success-*' | sort -n | tail -n 1`
-cd ..
-git add uaa
-echo `git commit -m "Auto-updating UAA to latest develop version"`
+LAST_TRAVIS_SUCCESS=`git tag -l 'travis-success-*' | sort -n | tail -n 1`
+COMMITS_BEHIND=`git log --oneline HEAD..${LAST_TRAVIS_SUCCESS} | grep -e '^' -c`
 
-set +x
-
-git push https://$GH_TOKEN:x-oauth-basic@github.com/cloudfoundry/login-server.git develop 2>&1 | grep -v $GH_TOKEN
+if [ $COMMITS_BEHIND -gt 0 ]
+then
+    echo "UAA is ${COMMITS_BEHIND} commits behind the last successful UAA Travis commit."
+    echo "Auto-bumping submodule"
+    git checkout ${LAST_TRAVIS_SUCCESS}
+    cd ..
+    git add uaa
+    echo `git commit -m "Auto-updating UAA to latest develop version"`
+    set +x
+    git push https://$GH_TOKEN:x-oauth-basic@github.com/cloudfoundry/login-server.git develop 2>&1 | grep -v $GH_TOKEN
+else
+    echo "UAA is already at or ahead of the last successful travis build."
+    echo "Leaving submodule as is."
+fi
