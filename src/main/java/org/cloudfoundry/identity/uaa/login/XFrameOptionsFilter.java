0898a7184c554d283400623452c3a89e53765b49
Morgan Fine <mfine@pivotallabs.com>
2014-07-21 15:00:30 -0700

    Added full integration test for X-Frame-Options header.
    [#73734414] https://www.pivotaltracker.com/story/show/73734414
    
    Signed-off-by: Rob Gallagher <rgallagher@pivotallabs.com>

--- src/main/java/org/cloudfoundry/identity/uaa/login/XFrameOptionsFilter.java
+++ src/main/java/org/cloudfoundry/identity/uaa/login/XFrameOptionsFilter.java
@@ -1,6 +1,5 @@
 package org.cloudfoundry.identity.uaa.login;
 
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -11,16 +10,9 @@ import java.io.IOException;
 
 public class XFrameOptionsFilter extends OncePerRequestFilter {
 
-    private final XFrameOptionsHeaderWriter xFrameOptionsHeaderWriter;
-
-    public XFrameOptionsFilter(XFrameOptionsHeaderWriter xFrameOptionsHeaderWriter) {
-        super();
-        this.xFrameOptionsHeaderWriter = xFrameOptionsHeaderWriter;
-    }
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        xFrameOptionsHeaderWriter.writeHeaders(request, response);
+        response.addHeader("X-Frame-Options", "DENY");
         filterChain.doFilter(request, response);
     }
 }
