2b9f905a2582200c8bb3b1a9e7faa81904fe9f80
Filip Hanik <fhanik@pivotal.io>
2014-09-19 11:04:43 -0600

    Upgrade to Spring Security Oauth2 2.0.3-RELEASE https://www.pivotaltracker.com/story/show/76969388 [#76969388]
    Change package name for BaseClientDetails
    Upgrade Spring Security Oauth2 XSD
    Add in backwards compatible filter to parse scope parameters containing commas
    Replaces commas with spaces for backwards compatibility
    
    Fix Oauth2RestTemplate behavior change in Spring Security Oauth2
    https://github.com/fhanik/spring-security-oauth/commit/b3d55e44f85e672930831f02e3b4e888b9a9c511

--- src/main/java/org/cloudfoundry/identity/uaa/login/ClientInfoAuthenticationFilter.java
+++ src/main/java/org/cloudfoundry/identity/uaa/login/ClientInfoAuthenticationFilter.java
@@ -41,7 +41,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.provider.BaseClientDetails;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.ClientDetails;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
