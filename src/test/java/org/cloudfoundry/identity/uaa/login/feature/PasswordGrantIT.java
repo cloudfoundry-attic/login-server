85e8df6e63c459df08625d17494c3bbc0554cb12
Rob Gallagher <rgallagher@pivotal.io>
2014-11-21 09:32:36 -0800

    Remove test for unverified user from PasswordGrantIT - now tested in a mockmvc test
    Signed-off-by: Madhura Bhave <mbhave@pivotal.io>

--- src/test/java/org/cloudfoundry/identity/uaa/login/feature/PasswordGrantIT.java
+++ src/test/java/org/cloudfoundry/identity/uaa/login/feature/PasswordGrantIT.java
@@ -7,7 +7,6 @@ import org.cloudfoundry.identity.uaa.test.UaaTestAccounts;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,13 +20,9 @@ import org.springframework.security.oauth2.client.test.TestAccounts;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestOperations;
 
-import java.security.SecureRandom;
 import java.util.Arrays;
-
-import static org.cloudfoundry.identity.uaa.test.HasStatusCode.hasStatusCode;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = DefaultIntegrationTestConfig.class)
@@ -68,46 +63,5 @@ public class PasswordGrantIT {
             Void.class);
 
         Assert.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
-
-    @Test
-    public void testUnverifiedUserLoginViaPasswordGrant() throws Exception {
-        String userEmail = createUnverifiedUser();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.add("Authorization", ((UaaTestAccounts) testAccounts).getAuthorizationHeader("cf", ""));
-
-        LinkedMultiValueMap<String, String> postBody = new LinkedMultiValueMap<>();
-        postBody.add("grant_type", "password");
-        postBody.add("username", userEmail);
-        postBody.add("password", "secret");
-
-        expectedException.expect(HttpClientErrorException.class);
-        expectedException.expect(hasStatusCode(HttpStatus.FORBIDDEN));
-
-        restOperations.exchange(baseUrl + "/oauth/token",
-            HttpMethod.POST,
-            new HttpEntity<>(postBody, headers),
-            Void.class);
-    }
-
-    private String createUnverifiedUser() throws Exception {
-        int randomInt = new SecureRandom().nextInt();
-
-        String adminAccessToken = testClient.getOAuthAccessToken("admin", "adminsecret", "client_credentials", "clients.read clients.write clients.secret");
-
-        String scimClientId = "scim" + randomInt;
-        testClient.createScimClient(adminAccessToken, scimClientId);
-
-        String scimAccessToken = testClient.getOAuthAccessToken(scimClientId, "scimsecret", "client_credentials", "scim.read scim.write password.write");
-
-        String userEmail = "user" + randomInt + "@example.com";
-        testClient.createUser(scimAccessToken, userEmail, userEmail, "secret", false);
-
-        return userEmail;
     }
 }
