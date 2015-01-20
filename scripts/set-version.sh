d5b488145f391c20ee5617513506a56f0f4ac676
Madhura Bhave <mbhave@pivotal.io>
2015-01-20 10:21:45 -0800

    Make set-version.sh work on both Linux and MacOS

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
 
 cd `dirname $0`/..
+sed -e "s/^version=.*/version=$1/" gradle.properties > gradle.properties.new
+mv gradle.properties.new gradle.properties
 
-sed -i -E "s/^version=.+$/version=$1/" gradle.properties
